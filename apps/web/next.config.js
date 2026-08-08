216cb72c60815d2908da0db471d88961a80dbf83
mt-abdulla-it <mt.abdulla.it@gmail.com>
2026-08-08 22:58:18 +0530

    Resolve merge conflicts

---
+++ apps/web/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    workerThreads: false,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
